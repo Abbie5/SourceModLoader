00fd2c7bc8070d78c8a9c0fb2b948c1b02c29fae
Abbie <abigail@ient.me>
2023-07-30 18:32:00 +0100

    implement local dir mod source

--- src/main/java/cc/abbie/sourcemodloader/source/LocalDirModSource.java
+++ src/main/java/cc/abbie/sourcemodloader/source/LocalDirModSource.java
@@ -1,10 +1,15 @@
 package cc.abbie.sourcemodloader.source;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
 
 public final class LocalDirModSource extends ModSource {
 	@Override
-	public void downloadSource(File outDir) {
+	public void downloadSource(File outDir) throws IOException {
+		if (!url.getProtocol().equals("file")) throw new IOException("invalid protocol");
 
+		FileUtils.copyDirectory(new File(url.getPath()), outDir);
 	}
 }
